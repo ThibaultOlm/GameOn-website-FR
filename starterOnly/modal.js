84326249e87547d0ac90107580ce9a6adf461410
Thibault Oulmiere <thibault.oulmiere@gmail.com>
2021-08-06 11:10:54 +0200

    Test récupération des données du formulaire et affichage avec console.log

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,14 @@
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelector(".content .close");
+const modalIconResponsive = document.querySelector(".icon");
+
+/*---- Fonction permettant l'affichage du menu burger en responsive-----*/
+
+modalIconResponsive.addEventListener("click", editNav);
+
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,28 +18,49 @@ function editNav() {
   }
 }
 
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const modalClose = document.querySelector(".content .close");
-const modalIconResponsive = document.querySelector(".icon");
-const modalSubmit = document.querySelector(".btn-submit");
+
+/*---- Fonction permettant d'ouvrir et de fermer la fenetre modale-----*/
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+// Click to leave
 modalClose.addEventListener("click", closeModal);
-
-modalIconResponsive.addEventListener("click", editNav);
-
-modalSubmit.addEventListener("click", console.log("Test Submit OK"));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+// Close modal form
 function closeModal() {
   modalbg.style.display = "none";
 }
+
+/*----------------------- GESTION  DU FORMULAIRE -------------------------*/
+
+// Les variables 
+
+const form = document.querySelector("#formulaire");
+const firstName = document.querySelector("#first");
+const lastName = document.querySelector("#last");
+const email = document.querySelector("#email");
+const birthdayDate = document.querySelector("#birthdate");
+const quantityTournament = document.querySelector("#quantity");
+const modalSubmit = document.querySelector(".btn-submit");
+
+
+form.addEventListener('submit', submitValidateTest);
+
+function submitValidateTest(e) {
+
+  e.preventDefault();
+
+  console.log("Prénom : " + firstName.value);
+  console.log("Nom : " + lastName.value);
+  console.log("Adresse email : " + email.value);
+  console.log("Date de naissance : " + birthdayDate.value);
+
+}
+
+
